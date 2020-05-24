d221610c707839f5d07b6e49a767cf69dae72dce
maaush <mauribenitez95@gmail.com>
2020-05-23 23:22:27 -0300

    add Frontend in react

---
+++ build/precache-manifest.72db32d1ebbb6bee0e7345ca63c5f950.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07a5f3ad8c8b77a53cbb8db5ba743c7e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9520ec10cd82d92716f3",
+    "url": "/static/css/main.363f1ee6.chunk.css"
+  },
+  {
+    "revision": "a89ef33c33598b7c56cb",
+    "url": "/static/js/2.c52fba94.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c52fba94.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9520ec10cd82d92716f3",
+    "url": "/static/js/main.a4af1ee4.chunk.js"
+  },
+  {
+    "revision": "3a382ed781e7be666fb3",
+    "url": "/static/js/runtime-main.bdc95ec2.js"
+  }
+]);
