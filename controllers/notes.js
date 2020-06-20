49117b1fead5b6634c7eba62a71bdbd8a27e9737
maaush <mauribenitez95@gmail.com>
2020-06-20 15:47:52 -0300

    Bugfixing on PUT method.

--- controllers/notes.js
+++ controllers/notes.js
@@ -1,29 +1,25 @@
-const notesRouter = require('express').Router();
-const User = new require('../models/user');
-const Note = new require('../models/note');
-const jwt = require('jsonwebtoken');
+const notesRouter = require("express").Router();
+const User = require("../models/user");
+const Note = require("../models/note");
+const jwt = require("jsonwebtoken");
 
 const getTokenFrom = (request) => {
-  const authorization = request.get('authorization');
+  const authorization = request.get("authorization");
 
-  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
-
+  if (authorization && authorization.toLowerCase().startsWith("bearer ")) {
     return authorization.substring(7);
   }
 
   return null;
-}
+};
 
-notesRouter.get('/', async (req, res) => {
+notesRouter.get("/", async (req, res) => {
+  const notes = await Note.find({}).populate("user", { username: 1, name: 1 });
 
-  const notes = await Note
-    .find({}).populate('user', { username: 1, name: 1 });
-
-  res.json(notes.map(note => note.toJSON()))
+  res.json(notes.map((note) => note.toJSON()));
 });
 
-notesRouter.get('/:id', async (req, res, next) => {
-
+notesRouter.get("/:id", async (req, res, next) => {
   const requestedNote = await Note.findById(req.params.id);
 
   if (requestedNote) {
@@ -33,15 +29,13 @@ notesRouter.get('/:id', async (req, res, next) => {
   }
 });
 
-notesRouter.post('/', async (req, res, next) => {
-
+notesRouter.post("/", async (req, res, next) => {
   const body = req.body;
   const token = getTokenFrom(req);
   const decodedToken = jwt.verify(token, process.env.SECRET);
 
   if (!token || !decodedToken) {
-
-    return res.status(401).json({ error: 'token missing or invalid' });
+    return res.status(401).json({ error: "token missing or invalid" });
   }
 
   const user = await User.findById(decodedToken.id);
@@ -54,28 +48,29 @@ notesRouter.post('/', async (req, res, next) => {
   });
 
   const savedNote = await note.save();
-  user.notes = user.notes.concat(savedNote._id)
+  user.notes = user.notes.concat(savedNote._id);
   await user.save();
   res.json(savedNote.toJSON);
 });
 
-notesRouter.delete('/:id', async (req, res, next) => {
-
-  await Note.findByIdAndRemove(req.params.id)
+notesRouter.delete("/:id", async (req, res, next) => {
+  await Note.findByIdAndRemove(req.params.id);
   res.status(204).end();
 });
 
-notesRouter.put('/:id', async (req, res, next) => {
-
+notesRouter.put("/:id", async (req, res, next) => {
   const body = req.body;
 
   const note = {
     content: body.content || " ",
-    important: body.important || false
+    important: body.important || false,
   };
 
-  const updatedNote = await Note.findByIdAndUpdate(req.params.id, note, {new: true});
-  res.json(updatedNote.toJSON);
+  const updatedNote = await Note.findByIdAndUpdate(req.params.id, note, {
+    new: true,
+  });
+
+  res.json(updatedNote.toJSON());
 });
 
 module.exports = notesRouter;
